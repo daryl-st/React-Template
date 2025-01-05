b64cb2399a47e15af30edb31680c66225e258b8b
Dawit Genene <Dawit.spark@gmail.com>
2025-01-05 10:30:04 +0300

    To-do list Challenge completed

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+    const [newTask, setTask] = useState("");
+
+    function handleChange(event) {
+        let task = event.target.value;
+        setTask(task);
+    }
+
+    const [tasks, setTasks] = useState([]);
+
+
+    function addTask(task) {
+        setTasks([task, ...tasks])        
+    }
+
+
     return (
-        <>
-            <h1>Custom React Template</h1>
-        </>
+        <div className="container">
+            <div className="heading">
+                <h1>To-Do List</h1>
+            </div>
+            <div className="form">
+                <input onChange={handleChange} type="text" name="task" value={newTask}/>
+                <button>
+                    <span onClick={() => {addTask(newTask)} }>Add</span>
+                </button>
+            </div>
+            <div>
+                <ul>  
+                    {tasks.map((task) => {
+                        return <li> {task} </li>
+                    })}
+                </ul>
+            </div>
+        </div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -6,3 +6,9 @@ const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
     <App />
 );
+
+//CHALLENGE: Make this app work by applying what you've learnt.
+//1. When new text is written into the input, its state should be saved....done
+//2. When the add button is pressed, the current data in the input should be
+//added to an array.
+//3. The <ul> should display all the array items as <li>s
